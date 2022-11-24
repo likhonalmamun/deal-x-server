638c36ede44931000aa5de23846da134cc791217
likhonalmamun <alikhon055@gmail.com>
2022-11-24 09:46:09 +0600

    connected database and added categories route

--- index.js
+++ index.js
@@ -2,15 +2,25 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 const jwt = require("jsonwebtoken");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const port = process.env.PORT || 5000;
 const app = express();
 app.use(cors());
 app.use(express.json());
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nbkl0so.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+console.log(uri);
 const run = () => {
   try {
-    app.get("/", async (req, res) => {
-      res.send({ message: "Server is running" });
+    const categoryCollection = client.db("dealX").collection("categories");
+    app.get("/categories", async (req, res) => {
+      const categories = await categoryCollection.find({}).toArray();
+      res.send(categories);
     });
   } catch (error) {
     console.log(error.message);
